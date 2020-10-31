6b1da4f9779833c8cd0bdc214a7c047c20571ee9
Harusari <uxdx@naver.com>
2020-11-01 00:10:28 +0900

    Init

---
+++ Ichimai/lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = 'a8c5bf6cfcac4f3a80c5e379becea87d';
+const Token =
+    '0062d1849cb5f03429a914a05b18443d112IAAt3YUihnTPrZk9dtk1kJ2piGgkMPg/p+22zmZ86O8V+qnYijUAAAAAEADJ+bHOQLOeXwEAAQA+s55f';
